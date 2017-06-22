aed12466e7c466076fc616ca4554cb5e4cc11ccc
HChong <hchong7557@gmail.com>
2017-06-22 20:50:19 +0800

    first commit
    Signed-off-by: HChong <hchong7557@gmail.com>

---
+++ AutoGetterPlugin/AutoGetterPlugin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoGetterPlugin
+//
+//  Created by HChong on 2017/6/22.
+//  Copyright © 2017年 HChong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
